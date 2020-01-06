07ad76846b87fa7a385083522c79d21ff75f7c13
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-01-06 13:28:29 +0100

    added depcheck

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -32,12 +32,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
+        use: ["babel-loader"]
       }
     ]
   },
