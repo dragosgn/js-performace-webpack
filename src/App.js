98ee8940f937f29730be6af2ba8f03b1720f744f
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-01-06 14:13:31 +0100

    fixed webpack worker, etc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-const App = ({ title }) => <div>{title}</div>;
+const runWorker = async () => {
+  const worker = new Worker();
+  const message = await new Promise((resolve, reject) => {
+    worker.addEventListener(
+      'message',
+      event => resolve(event.data),
+      false
+    );
+    worker.addEventListener('error', reject, false);
+  });
+  return message;
+};
+
+const App = ({ title }) => {
+  const [data, setData] = useState({ workerCalculation: null });
+
+  useEffect(async () => {
+    const result = await runWorker();
+    setData(result);
+  }, []);
+
+  return (
+    <div>
+      <div>{title}</div>
+      <p>{data ? data : 'Worker is working....'}</p>
+    </div>
+  );
+};
 
 export default App;
